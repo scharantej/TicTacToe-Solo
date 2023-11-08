7ede3a6a4fa7066e27535732648d1d2f5020861a
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-08 20:49:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,131 +1,229 @@
  
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Define the game board
-board = [['-', '-', '-'],
-         ['-', '-', '-'],
-         ['-', '-', '-']]
-
-# Define the possible winning combinations
-winning_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-                       [0, 3, 6], [1, 4, 7], [2, 5, 8],
-                       [0, 4, 8], [2, 4, 6]]
-
-# Define the computer player
-computer_player = 'O'
-
-# Define the human player
-human_player = 'X'
-
-# Define the current player
-current_player = human_player
-
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
-
-@app.route('/game', methods=['POST'])
+@app.route('/game')
 def game():
-    # Get the move from the human player
-    human_move = request.form.get('move')
-
-    # Update the game board
-    board[int(human_move[0])][int(human_move[1])] = human_player
-
-    # Check if the human player has won
-    if has_won(human_player):
-        return redirect(url_for('winner'))
-
-    # Check if the game is a tie
-    if is_tie():
-        return redirect(url_for('tie'))
-
-    # Get the move from the computer player
-    computer_move = get_computer_move()
-
-    # Update the game board
-    board[int(computer_move[0])][int(computer_move[1])] = computer_player
-
-    # Check if the computer player has won
-    if has_won(computer_player):
-        return redirect(url_for('winner'))
-
-    # Check if the game is a tie
-    if is_tie():
-        return redirect(url_for('tie'))
-
-    # Return the updated game board
-    return render_template('game.html', board=board)
-
+  return render_template('game.html')
 
 @app.route('/winner')
 def winner():
-    return render_template('winner.html', winner=current_player)
-
-
-@app.route('/tie')
-def tie():
-    return render_template('tie.html')
-
-
-@app.route('/reset')
-def reset():
-    # Reset the game board
-    board = [['-', '-', '-'],
-             ['-', '-', '-'],
-             ['-', '-', '-']]
-
-    # Reset the current player
-    current_player = human_player
-
-    # Redirect to the home page
-    return redirect(url_for('index'))
-
-
-def has_won(player):
-    # Check if the player has won
-    for winning_combination in winning_combinations:
-        if all(board[i][j] == player for i, j in winning_combination):
-            return True
-    return False
-
-
-def is_tie():
-    # Check if the game is a tie
-    for i in range(3):
-        if board[i][0] != '-' and board[i][1] != '-' and board[i][2] != '-':
-            return False
-        if board[0][i] != '-' and board[1][i] != '-' and board[2][i] != '-':
-            return False
-        if board[i][0] != '-' and board[1][1] != '-' and board[2][2] != '-':
-            return False
-        if board[0][2] != '-' and board[1][1] != '-' and board[2][0] != '-':
-            return False
-    return True
-
-
-def get_computer_move():
-    # Get the computer's move
-    for winning_combination in winning_combinations:
-        if all(board[i][j] == computer_player for i, j in winning_combination):
-            return winning_combination[0], winning_combination[1]
-        if all(board[i][j] == human_player for i, j in winning_combination):
-            return winning_combination[0], winning_combination[1]
-    for i in range(3):
-        if board[i][0] == '-' and board[i][1] == '-' and board[i][2] == '-':
-            return i, 0
-        if board[0][i] == '-' and board[1][i] == '-' and board[2][i] == '-':
-            return 0, i
-        if board[i][0] == '-' and board[1][1] == '-' and board[2][2] == '-':
-            return i, i
-        if board[0][2] == '-' and board[1][1] == '-' and board[2][0] == '-':
-            return i, 2
-    return 1, 1
-
+  return render_template('winner.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run()
+
+
+HTML files
+
+html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>TicTacToe</title>
+  <link rel="stylesheet" href="/static/style.css">
+</head>
+<body>
+  <h1>TicTacToe</h1>
+  <button onclick="startGame()">Start Game</button>
+
+  <script src="/static/script.js"></script>
+</body>
+</html>
+
+
+html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>TicTacToe</title>
+  <link rel="stylesheet" href="/static/style.css">
+</head>
+<body>
+  <h1>TicTacToe</h1>
+
+  <div id="game-board">
+    <div class="square" onclick="placePiece(0, 0)"></div>
+    <div class="square" onclick="placePiece(0, 1)"></div>
+    <div class="square" onclick="placePiece(0, 2)"></div>
+    <div class="square" onclick="placePiece(1, 0)"></div>
+    <div class="square" onclick="placePiece(1, 1)"></div>
+    <div class="square" onclick="placePiece(1, 2)"></div>
+    <div class="square" onclick="placePiece(2, 0)"></div>
+    <div class="square" onclick="placePiece(2, 1)"></div>
+    <div class="square" onclick="placePiece(2, 2)"></div>
+  </div>
+
+  <script src="/static/script.js"></script>
+</body>
+</html>
+
+
+html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>TicTacToe</title>
+  <link rel="stylesheet" href="/static/style.css">
+</head>
+<body>
+  <h1>TicTacToe</h1>
+
+  <p>You won!</p>
+
+  <button onclick="startGame()">Start a new game</button>
+
+  <script src="/static/script.js"></script>
+</body>
+</html>
+
+
+CSS
+
+css
+body {
+  font-family: sans-serif;
+}
+
+h1 {
+  font-size: 36px;
+}
+
+button {
+  font-size: 16px;
+  padding: 8px 16px;
+  background-color: #000;
+  color: #fff;
+}
+
+.square {
+  width: 50px;
+  height: 50px;
+  border: 1px solid #000;
+  float: left;
+  margin: 4px;
+}
+
+
+JavaScript
+
+javascript
+function startGame() {
+  // Initialize the game board.
+  var gameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+
+  // Render the game board.
+  renderGameBoard(gameBoard);
+}
+
+function placePiece(row, col) {
+  // Check if the square is already occupied.
+  if (gameBoard[row][col] !== null) {
+    return;
+  }
+
+  // Place the piece on the game board.
+  gameBoard[row][col] = 'X';
+
+  // Render the game board.
+  renderGameBoard(gameBoard);
+
+  // Check if the game is over.
+  if (checkGameOver(gameBoard)) {
+    // The game is over. Display the winner.
+    alert('You won!');
+  }
+}
+
+function renderGameBoard(gameBoard) {
+  // Get the game board element.
+  var gameBoardElement = document.getElementById('game-board');
+
+  // Clear the game board element.
+  gameBoardElement.innerHTML = '';
+
+  // Loop through the game board and create a square for each element.
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      var square = document.createElement('div');
+      square.className = 'square';
+      square.onclick = function() {
+        placePiece(i, j);
+      };
+
+      // Set the square's inner HTML to the value of the game board element.
+      square.innerHTML = gameBoard[i][j];
+
+      // Append the square to the game board element.
+      gameBoardElement.appendChild(square);
+    }
+  }
+}
+
+function checkGameOver(gameBoard) {
+  // Check if there is a winner.
+  for (var i = 0; i < 3; i++) {
+    if (gameBoard[i][0] === 'X' && gameBoard[i][1] === 'X' && gameBoard[i][2] === 'X') {
+      return true;
+    }
+    if (gameBoard[0][i] === 'X' && gameBoard[1][i] === 'X' && gameBoard[2][i] === 'X') {
+      return true;
+    }
+    if (gameBoard[i][0] === 'X' && gameBoard[1][1] === 'X' && gameBoard[2][2] === 'X') {
+      return true;
+    }
+    if (gameBoard[0][2] === 'X' && gameBoard[1][1] === 'X' && gameBoard[2][0] === 'X') {
+      return true;
+    }
+  }
+
+  // Check if there is a tie.
+  for (var i = 0; i < 3; i++) {
+    if (gameBoard[i][0] !== null && gameBoard[i][1] !== null && gameBoard[i][2] !== null) {
+      return false;
+    }
+    if (gameBoard[0][i] !== null && gameBoard[1][i] !== null && gameBoard[2][i] !== null) {
+      return false;
+    }
+    if (gameBoard[i][0] !== null && gameBoard[1][1] !== null && gameBoard[2][2] !== null) {
+      return false;
+    }
+    if (gameBoard[0][2] !== null && gameBoard[1][1] !== null && gameBoard[2][0] !== null) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+main.py
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/game')
+def game():
+  return render_template('game.html')
+
+@app.route('/winner')
+def winner():
+  return render_template('winner.html')
+
+if __name__ == '__main__':
+  app.run()
