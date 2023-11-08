c79badd4499864da50e5a980bc7e9f2d7a83c47b
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-08 19:28:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,130 +3,100 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'POST':
+        difficulty = request.form.get('difficulty')
+        return redirect(url_for('game', difficulty=difficulty))
+    return render_template('index.html')
 
-@app.route('/game', methods=['POST'])
+@app.route('/game', methods=['GET', 'POST'])
 def game():
-  # Get the user's move.
-  move = request.form.get('move')
+    difficulty = request.args.get('difficulty')
+    if request.method == 'POST':
+        move = request.form.get('move')
+        # Make the computer's move
+        computer_move = make_computer_move(difficulty)
+        # Check if the game is over
+        winner = check_winner()
+        if winner:
+            return redirect(url_for('winner', winner=winner))
+        # Render the game board
+        return render_template('game.html', difficulty=difficulty, board=board, move=move, computer_move=computer_move)
+    return render_template('game.html', difficulty=difficulty, board=board)
 
-  # Make the computer's move.
-  computer_move = get_computer_move()
-
-  # Check if the game is over.
-  winner = check_winner()
-
-  # Render the game page.
-  return render_template('game.html', move=move, computer_move=computer_move, winner=winner)
-
-@app.route('/winner', methods=['POST'])
+@app.route('/winner', methods=['GET'])
 def winner():
-  # Get the winner.
-  winner = request.form.get('winner')
+    winner = request.args.get('winner')
+    return render_template('winner.html', winner=winner)
 
-  # Render the winner page.
-  return render_template('winner.html', winner=winner)
-
-def get_computer_move():
-  # Get the available moves.
-  available_moves = get_available_moves()
-
-  # Choose a random move.
-  computer_move = random.choice(available_moves)
-
-  return computer_move
+def make_computer_move(difficulty):
+    # Choose a random move
+    if difficulty == 'easy':
+        move = random.choice(available_moves())
+    # Choose the best move
+    elif difficulty == 'hard':
+        move = minimax(board, depth=3)
+    return move
 
 def check_winner():
-  # Check if there is a winner.
-  winner = None
+    # Check if there is a winner
+    for row in range(3):
+        if board[row][0] == board[row][1] == board[row][2] != ' ':
+            return board[row][0]
+        if board[0][row] == board[1][row] == board[2][row] != ' ':
+            return board[0][row]
+        if board[row][0] == board[1][1] == board[2][2] != ' ':
+            return board[row][0]
+        if board[2][0] == board[1][1] == board[0][2] != ' ':
+            return board[2][0]
+    return None
 
-  # Check if there is a row with three of the same symbol.
-  for row in range(3):
-    if board[row][0] == board[row][1] == board[row][2]:
-      winner = board[row][0]
+def available_moves():
+    # Return a list of available moves
+    moves = []
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == ' ':
+                moves.append((row, col))
+    return moves
 
-  # Check if there is a column with three of the same symbol.
-  for col in range(3):
-    if board[0][col] == board[1][col] == board[2][col]:
-      winner = board[0][col]
+def minimax(board, depth, alpha=-float('inf'), beta=float('inf')):
+    # Check if the game is over
+    winner = check_winner()
+    if winner:
+        if winner == 'X':
+            return -1
+        elif winner == 'O':
+            return 1
+        else:
+            return 0
 
-  # Check if there is a diagonal with three of the same symbol.
-  if board[0][0] == board[1][1] == board[2][2]:
-    winner = board[0][0]
-  elif board[0][2] == board[1][1] == board[2][0]:
-    winner = board[0][2]
-
-  return winner
-
-if __name__ == '__main__':
-  app.run()
-
-
-main.py
-
-
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return render_template('index.html')
-
-@app.route('/game', methods=['POST'])
-def game():
-  # Get the user's move.
-  move = request.form.get('move')
-
-  # Make the computer's move.
-  computer_move = get_computer_move()
-
-  # Check if the game is over.
-  winner = check_winner()
-
-  # Render the game page.
-  return render_template('game.html', move=move, computer_move=computer_move, winner=winner)
-
-@app.route('/winner', methods=['POST'])
-def winner():
-  # Get the winner.
-  winner = request.form.get('winner')
-
-  # Render the winner page.
-  return render_template('winner.html', winner=winner)
-
-def get_computer_move():
-  # Get the available moves.
-  available_moves = get_available_moves()
-
-  # Choose a random move.
-  computer_move = random.choice(available_moves)
-
-  return computer_move
-
-def check_winner():
-  # Check if there is a winner.
-  winner = None
-
-  # Check if there is a row with three of the same symbol.
-  for row in range(3):
-    if board[row][0] == board[row][1] == board[row][2]:
-      winner = board[row][0]
-
-  # Check if there is a column with three of the same symbol.
-  for col in range(3):
-    if board[0][col] == board[1][col] == board[2][col]:
-      winner = board[0][col]
-
-  # Check if there is a diagonal with three of the same symbol.
-  if board[0][0] == board[1][1] == board[2][2]:
-    winner = board[0][0]
-  elif board[0][2] == board[1][1] == board[2][0]:
-    winner = board[0][2]
-
-  return winner
+    # Check if the current player is maximizing or minimizing
+    if depth % 2 == 0:
+        # Maximizing player
+        best_score = -float('inf')
+        for move in available_moves():
+            board[move[0]][move[1]] = 'X'
+            score = minimax(board, depth + 1, alpha, beta)
+            board[move[0]][move[1]] = ' '
+            best_score = max(best_score, score)
+            alpha = max(alpha, best_score)
+            if beta <= alpha:
+                break
+        return best_score
+    else:
+        # Minimizing player
+        best_score = float('inf')
+        for move in available_moves():
+            board[move[0]][move[1]] = 'O'
+            score = minimax(board, depth + 1, alpha, beta)
+            board[move[0]][move[1]] = ' '
+            best_score = min(best_score, score)
+            beta = min(beta, best_score)
+            if beta <= alpha:
+                break
+        return best_score
 
 if __name__ == '__main__':
-  app.run()
+    app.run(debug=True)
