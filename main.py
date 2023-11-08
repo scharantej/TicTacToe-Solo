b330751d7b8f8e93343ec2605760387142f320a4
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-08 19:09:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ human_player = 'O'
 # Define the current player
 current_player = human_player
 
+# Define the game state
+game_state = 'in progress'
+
 
 @app.route('/')
 def index():
@@ -30,29 +33,42 @@ def index():
 
 @app.route('/game', methods=['GET', 'POST'])
 def game():
-    # Get the move from the human player
+    # Get the user's move
     if request.method == 'POST':
         move = request.form.get('move')
         board[int(move[0])][int(move[1])] = human_player
 
         # Check if the human player has won
         if check_winner(board, human_player):
+            game_state = 'human_won'
             return render_template('winner.html', winner=human_player)
 
-        # Get the move from the computer player
-        move = get_computer_move(board)
-        board[int(move[0])][int(move[1])] = computer_player
+        # Check if the game is a draw
+        if check_draw(board):
+            game_state = 'draw'
+            return render_template('winner.html', winner='draw')
+
+        # Make the computer's move
+        computer_move = get_computer_move(board)
+        board[computer_move[0]][computer_move[1]] = computer_player
 
         # Check if the computer player has won
         if check_winner(board, computer_player):
+            game_state = 'computer_won'
             return render_template('winner.html', winner=computer_player)
 
-    # Check if the game is a draw
-    if check_draw(board):
-        return render_template('winner.html', winner='Draw')
+        # Check if the game is a draw
+        if check_draw(board):
+            game_state = 'draw'
+            return render_template('winner.html', winner='draw')
 
     # Render the game board
-    return render_template('game.html', board=board)
+    return render_template('game.html', board=board, current_player=current_player)
+
+
+@app.route('/winner')
+def winner():
+    return render_template('winner.html', winner=game_state)
 
 
 def check_winner(board, player):
@@ -77,37 +93,19 @@ def check_draw(board):
 
 def get_computer_move(board):
     for winning_combination in winning_combinations:
-        if board[winning_combination[0]][winning_combination[1]] == computer_player and board[winning_combination[2]][winning_combination[3]] == '-':
-            return winning_combination[2], winning_combination[3]
-        if board[winning_combination[0]][winning_combination[2]] == computer_player and board[winning_combination[1]][winning_combination[3]] == '-':
-            return winning_combination[1], winning_combination[3]
-        if board[winning_combination[1]][winning_combination[2]] == computer_player and board[winning_combination[0]][winning_combination[3]] == '-':
-            return winning_combination[0], winning_combination[3]
-
-    for winning_combination in winning_combinations:
-        if board[winning_combination[0]][winning_combination[1]] == human_player and board[winning_combination[2]][winning_combination[3]] == '-':
-            return winning_combination[2], winning_combination[3]
-        if board[winning_combination[0]][winning_combination[2]] == human_player and board[winning_combination[1]][winning_combination[3]] == '-':
-            return winning_combination[1], winning_combination[3]
-        if board[winning_combination[1]][winning_combination[2]] == human_player and board[winning_combination[0]][winning_combination[3]] == '-':
-            return winning_combination[0], winning_combination[3]
-
+        if all(board[i][j] == computer_player for i, j in winning_combination):
+            return winning_combination[0], winning_combination[1]
+        if all(board[i][j] == human_player for i, j in winning_combination):
+            return winning_combination[0], winning_combination[1]
     for i in range(3):
-        if board[i][0] == '-' and board[i][1] != '-' and board[i][2] != '-':
+        if board[i][0] == '-' and board[i][1] == '-' and board[i][2] == '-':
             return i, 0
-        if board[i][1] == '-' and board[i][0] != '-' and board[i][2] != '-':
-            return i, 1
-        if board[i][2] == '-' and board[i][0] != '-' and board[i][1] != '-':
+        if board[0][i] == '-' and board[1][i] == '-' and board[2][i] == '-':
+            return 0, i
+        if board[i][0] == '-' and board[1][1] == '-' and board[2][2] == '-':
+            return i, i
+        if board[0][2] == '-' and board[1][1] == '-' and board[2][0] == '-':
             return i, 2
-
-    for j in range(3):
-        if board[0][j] == '-' and board[1][j] != '-' and board[2][j] != '-':
-            return 0, j
-        if board[1][j] == '-' and board[0][j] != '-' and board[2][j] != '-':
-            return 1, j
-        if board[2][j] == '-' and board[0][j] != '-' and board[1][j] != '-':
-            return 2, j
-
     return 1, 1
 
 
