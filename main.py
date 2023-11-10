48f1f2a6fe4601d83920da758e311c528f1f3b0e
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-11 01:21:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,78 +3,61 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+# Define the routes for the application
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        difficulty = request.form.get('difficulty')
-        return redirect(url_for('game', difficulty=difficulty))
     return render_template('index.html')
 
 @app.route('/game', methods=['GET', 'POST'])
 def game():
-    difficulty = request.args.get('difficulty')
-    if request.method == 'POST':
+    if request.method == 'GET':
+        # Initialize the game state
+        game_state = {
+            'board': [['', '', ''], ['', '', ''], ['', '', '']],
+            'current_player': 'X',
+            'winner': None
+        }
+        return render_template('game.html', game_state=game_state)
+    elif request.method == 'POST':
+        # Handle the user's move
         move = request.form.get('move')
-        # Make the computer's move
-        computer_move = make_computer_move(difficulty)
+        game_state['board'][move[0]][move[1]] = game_state['current_player']
+
         # Check if the game is over
-        winner = check_winner()
-        if winner:
-            return redirect(url_for('winner', winner=winner))
-        # Render the game board
-        return render_template('game.html', difficulty=difficulty, board=board)
-    return render_template('game.html', difficulty=difficulty, board=board)
+        if check_winner(game_state['board']):
+            game_state['winner'] = check_winner(game_state['board'])
+            return render_template('winner.html', game_state=game_state)
+
+        # Switch to the next player
+        game_state['current_player'] = 'O' if game_state['current_player'] == 'X' else 'X'
+
+        return render_template('game.html', game_state=game_state)
 
 @app.route('/winner', methods=['GET'])
 def winner():
-    winner = request.args.get('winner')
-    return render_template('winner.html', winner=winner)
+    game_state = request.args.get('game_state')
+    return render_template('winner.html', game_state=game_state)
 
-def make_computer_move(difficulty):
-    # Choose a random move
-    if difficulty == 'easy':
-        move = random.choice(available_moves())
-    # Choose the best move
-    elif difficulty == 'hard':
-        move = minimax(board, depth=3)
-    return move
-
-def check_winner():
-    # Check if there is a winner
+# Define the function to check for a winner
+def check_winner(board):
+    # Check for a winner in each row
     for row in range(3):
-        if board[row][0] == board[row][1] == board[row][2] != ' ':
+        if board[row][0] == board[row][1] == board[row][2] != '':
             return board[row][0]
-        if board[0][row] == board[1][row] == board[2][row] != ' ':
-            return board[0][row]
-        if board[row][0] == board[1][1] == board[2][2] != ' ':
-            return board[row][0]
-        if board[2][0] == board[1][1] == board[0][2] != ' ':
-            return board[2][0]
+
+    # Check for a winner in each column
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != '':
+            return board[0][col]
+
+    # Check for a winner in each diagonal
+    if board[0][0] == board[1][1] == board[2][2] != '':
+        return board[0][0]
+    elif board[0][2] == board[1][1] == board[2][0] != '':
+        return board[0][2]
+
     return None
 
-def available_moves():
-    # Return a list of available moves
-    moves = []
-    for row in range(3):
-        for col in range(3):
-            if board[row][col] == ' ':
-                moves.append((row, col))
-    return moves
-
-def minimax(board, depth):
-    # Return the best move for the computer
-    if depth == 0 or check_winner():
-        return None
-    best_move = None
-    best_score = -float('inf')
-    for move in available_moves():
-        board[move[0]][move[1]] = 'O'
-        score = -minimax(board, depth-1)
-        board[move[0]][move[1]] = ' '
-        if score > best_score:
-            best_move = move
-            best_score = score
-    return best_move
-
+# Run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
